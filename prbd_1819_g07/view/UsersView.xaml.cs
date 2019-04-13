e4db8b13cb4a660f00afefb498feac3c1fd91332
Matilde Gravano <matilde.gravano@gmail.Com>
2019-04-13 04:34:58 +0200

    Mise en image UsersView

---
+++ prbd_1819_g07/view/UsersView.xaml.cs
@@ -0,0 +1,64 @@
+﻿using PRBD_Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prbd_1819_g07
+{
+    /// <summary>
+    /// Logique d'interaction pour UsersView.xaml
+    /// </summary>
+    public partial class UsersView : UserControlBase
+    {
+        public ICommand NewUser { get; set; }
+
+        public UsersView()
+        {
+            InitializeComponent();
+
+            if (DesignerProperties.GetIsInDesignMode(this))
+                return;
+
+            DataContext = this;
+            Users = new ObservableCollection<User>(App.Model.Users);
+        }
+
+        private ObservableCollection<User> users;
+        public ObservableCollection<User> Users
+        {
+
+            get => users;
+
+            set => SetProperty<ObservableCollection<User>>(ref users, value, () =>
+            {
+            });
+
+        }
+
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListView.SelectedIndex;
+            switch (index)
+            {
+                case 0:
+
+
+                    break;
+            }
+        }
+    }
+}
